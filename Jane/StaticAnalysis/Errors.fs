69cb2b1d68513a7a999ad269dc5e1b2bd1a28ae7
Лозов Пётр <not_kill@mail.ru>
2014-05-08 14:32:20 +0400

    Update SA.

--- Jane/StaticAnalysis/Errors.fs
+++ Jane/StaticAnalysis/Errors.fs
@@ -1,15 +1,15 @@
-﻿module Errors
+﻿module internal SA.Errors
 
 open AST
-    
 
+// Дополнение для класса Error из AST    
 type Error with
 
-    static member DuplicateNode (kindNode : string) (nameNode : string) (positionNode : Position) = 
-        new Error(sprintf "Duplicate %s: %s." kindNode nameNode, positionNode)
+    static member DuplicateNode (kindNode : string) (nodeName : ID) = 
+        new Error(sprintf "Duplicate %s: \"%A\"." kindNode nodeName, nodeName.Position)
 
     static member MainClassNotFound (p : Program) =
-        new Error(sprintf "Main class is \"%s\" not found." p.NameMainClass, p.Position)
+        new Error(sprintf "Main class \"%s\" is not found." p.NameMainClass, p.Position)
 
     static member MainNotFound (c : Class) =
         new Error(sprintf "Method \"main\" is not found in the main class \"%s\"." c.Name.Value, c.Position)
@@ -24,10 +24,10 @@ type Error with
         new Error(sprintf "Method \"main\" must be void method.", cn.Position)
 
     static member IncorrectName (name : string) (position : Position) =
-        new Error(sprintf "Incorrect identifier: %s." name, position)
+        new Error(sprintf "Incorrect identifier: \"%s\"." name, position)
 
     static member ObjectIsNotExist (name : string) (position : Position) (typeObject : string) =
-        new Error (sprintf "%s with name %s not exists." typeObject name, position)
+        new Error (sprintf "%s with name \"%s\" not exists." typeObject name, position)
 
     static member CycleInheritIinterface (id : ID)=
-        new Error(sprintf "Interface %s iherits in cycle." id.Value, id.Position)
+        new Error(sprintf "Interface \"%s\" iherits in cycle." id.Value, id.Position)
---
+++ Jane/StaticAnalysis/HelperFunctions.fs
@@ -0,0 +1,42 @@
+﻿module internal SA.HelperFunctions
+
+open AST
+open SA.Errors
+
+// Добавляет по ID списку ошибки дублирования узлов в program
+let DuplicateNode (program : Program) (listNodes : ID list) (kindNode : string) =
+    
+    // В отсортированном списке ищет повторы и добавляет ошибки
+    let rec searchDuplicate (listNodes : ID list) (prev : ID) =
+        match listNodes with
+            | node :: lns when node.Value = prev.Value -> program.AddError <| Error.DuplicateNode kindNode node
+                                                          searchDuplicate lns node
+            | node :: lns                              -> searchDuplicate lns node
+            | []                                       -> ()
+
+    // Сортирует список по именам
+    let sortedListNodes = List.sortBy (fun (node : ID) -> node.Value) listNodes
+
+    // Проверяет сотсортированный список на пустоту и начинает поиск повторов
+    match sortedListNodes with
+    | node :: lns -> searchDuplicate lns node
+    | []          -> ()
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+// Формирует ошибки корректности идентификатора
+let IncorrectNameErrors (program : Program) (name : string) (p : Position) =
+    
+    // Список запрещённых идентификаторов
+    let listIncorrectNames = ["class"; "interface"; "extends"; "implements"; "static"; "final"; "if"; "else"; 
+                              "while"; "for"; "break"; "continue"; "return"; "super"; "this"; "null"; "new";
+                              "byte"; "short"; "int"; "long"; "float"; "double"; "char"; "string"; "boolean";
+                              "false"; "true"; "instanceOf"; "void"]
+
+    // Проверка на корректность
+    let incorrectName = List.tryFind ((=) name) listIncorrectNames
+
+    // Добавление ошибки
+    match incorrectName with
+    | Some str -> program.AddError <| Error.IncorrectName str p
+    | None     -> ()
