da0835b7dd2885644894bcc67a96a9499457537f
Nikita Batov <nikitabatov@gmail.com>
2014-05-29 00:19:45 +0400

    runtime + fixing

--- Jane/JaneIDE/View/CodeBox.xaml.cs
+++ Jane/JaneIDE/View/CodeBox.xaml.cs
@@ -22,6 +22,7 @@ namespace JaneIDE.View
     /// </summary>
     public partial class CodeBox : UserControl
     {
+        public ScintillaNET.Scintilla codeBox; 
         public CodeBox()
         {
             InitializeComponent();
@@ -29,75 +30,107 @@ namespace JaneIDE.View
             // Initialize CodeBox by ScintillaNET
 
             // Integration fix from official instruction
-            ScintillaNET.Scintilla CodeBox = (ScintillaNET.Scintilla)wfh.Child;
+            codeBox = (ScintillaNET.Scintilla)wfh.Child;
 
             this.DataContextChanged += new DependencyPropertyChangedEventHandler(Reload);
 
-            // Codebox configuration from JaNE.xml
-            CodeBox.ConfigurationManager.Language = "jane";
-            CodeBox.ConfigurationManager.CustomLocation = @"JaNE.xml";
-            CodeBox.ConfigurationManager.Configure();
+            // codeBox configuration from JaNE.xml
+            codeBox.ConfigurationManager.Language = "jane";
+            codeBox.ConfigurationManager.CustomLocation = @"JaNE.xml";
+            codeBox.ConfigurationManager.Configure();
 
-            // Codebox's margins configuration for string numbers and code folding
-            CodeBox.Margins[0].Width = 25; 
-            CodeBox.Margins[2].Width = 20;
+            // codeBox's margins configuration for string numbers and code folding
+            codeBox.Margins[0].Width = 25; 
+            codeBox.Margins[2].Width = 20;
 
-            // CodeBox's autocomplete
-            CodeBox.CharAdded += CodeBox_CharAdded;
-            CodeBox.AutoComplete.MaxWidth = 20;
-            CodeBox.AutoComplete.MaxHeight = 10;
-            CodeBox.AutoComplete.List.Sort();
+            // codeBox's autocomplete
+            codeBox.CharAdded += codeBox_CharAdded;
+            codeBox.AutoComplete.MaxWidth = 20;
+            codeBox.AutoComplete.MaxHeight = 10;
+            codeBox.AutoComplete.List.Sort();
 
             // Highlighting current line (light grey)
-            CodeBox.Caret.HighlightCurrentLine = true;
-            CodeBox.Caret.CurrentLineBackgroundColor = System.Drawing.Color.FromArgb(245, 245, 245);
+            codeBox.Caret.HighlightCurrentLine = true;
+            codeBox.Caret.CurrentLineBackgroundColor = System.Drawing.Color.FromArgb(245, 245, 245);
 
             //Code folding from default lexer
-            CodeBox.Folding.IsEnabled = true;
-            CodeBox.Folding.UseCompactFolding = true;
+            codeBox.Folding.IsEnabled = true;
+            codeBox.Folding.UseCompactFolding = true;
 
             //Background color
-            CodeBox.Selection.BackColor = System.Drawing.Color.FromArgb(190, 190, 190);
+            codeBox.Selection.BackColor = System.Drawing.Color.FromArgb(190, 190, 190);
 
             //Brace Matching (red and bold)
-            CodeBox.IsBraceMatching = true;
+            codeBox.IsBraceMatching = true;
 
             //Handler for snippets
-            CodeBox.KeyDown += CodeBox_KeyDown;
+            codeBox.KeyDown += codeBox_KeyDown;
         }
 
         private void Reload(object sender, DependencyPropertyChangedEventArgs e)
         {
             if (this.DataContext == null)
                 return;
-            var viewModel = (CodeBoxViewModel)this.DataContext;
-            ScintillaNET.Scintilla CodeBox = (ScintillaNET.Scintilla)wfh.Child; 
-            CodeBox.Text = viewModel.CodeboxText;
+            var viewModel = (CodeBoxViewModel)this.DataContext; 
+            codeBox.Text = viewModel.CodeboxText;
         }
 
-        void CodeBox_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
+        void codeBox_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
         {
             if (e.Alt && e.KeyCode == System.Windows.Forms.Keys.Space && e.Control)
             {
-                ((ScintillaNET.Scintilla)wfh.Child).Snippets.ShowSnippetList();
+                codeBox.Snippets.ShowSnippetList();
             }
         }
 
-        void CodeBox_CharAdded(object sender, ScintillaNET.CharAddedEventArgs e)
+        void codeBox_CharAdded(object sender, ScintillaNET.CharAddedEventArgs e)
         {
            
             if (Char.IsLetter(e.Ch))
-            ((ScintillaNET.Scintilla)wfh.Child).AutoComplete.Show();
+            codeBox.AutoComplete.Show();
         }
 
-        private void CodeBox_TextChanged(object sender, EventArgs e)
+        private void codeBox_TextChanged(object sender, EventArgs e)
         {
             var viewModel = (CodeBoxViewModel)this.DataContext;
             ScintillaNET.Scintilla CodeBox = sender as ScintillaNET.Scintilla;
-            string codeboxtext = CodeBox.Text;
+            string codeboxtext = codeBox.Text;
             viewModel.CodeboxText = codeboxtext;
             //TODO: BALLOON with ' '
         }
 
+        void ErrorMarker(int stringNum)
+        {
+            Marker marker = codeBox.Markers[0];
+            marker.Symbol = MarkerSymbol.Circle;
+            marker.BackColor = System.Drawing.Color.Red;
+            codeBox.Lines[stringNum - 1].AddMarker(marker);
+        }
+        void BreakPointMarker(int stringNum)
+        {
+            Marker marker = codeBox.Markers[1];
+            marker.Symbol = MarkerSymbol.FullRectangle;
+            marker.BackColor = System.Drawing.Color.Blue;
+            codeBox.Lines[stringNum - 1].AddMarker(marker);
+        }
+        void CurrentMarker(int stringNum)
+        {
+            Marker marker = codeBox.Markers[3];
+            marker.Symbol = MarkerSymbol.Arrow;
+            marker.BackColor = System.Drawing.Color.Green;
+            codeBox.Lines[stringNum - 1].AddMarker(marker);
+        }
+
+        void AddContext(List<string> context)
+        {
+            codeBox.AutoComplete.List.AddRange(context);
+            codeBox.AutoComplete.List = codeBox.AutoComplete.List.Distinct().ToList();
+            codeBox.AutoComplete.List.Sort();
+        }
+        void RemoveContext(List<string> context)
+        {
+            codeBox.AutoComplete.List = codeBox.AutoComplete.List.Except(context).ToList();
+            codeBox.AutoComplete.List.Sort();
+        } 
     }
 }
